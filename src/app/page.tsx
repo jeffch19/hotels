7d92f417591d006aee52554bf903179c33c7300c
jeffch19 <17jcummings@gmail.com>
2024-07-13 17:58:58 -0400

    show logged in user info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from '@clerk/nextjs/server';
 
-export default function Home() {
+export default async function Home() {
+  let clerkUserId = "";
+  let name = "";
+  let email = "";
+
+  const currentUserData = await currentUser();
+
+  if (currentUserData) {
+    clerkUserId = currentUserData.id;
+    name = currentUserData.firstName + " " + currentUserData.lastName;
+    email = currentUserData.emailAddresses[0].emailAddress;
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-5">
+    <div className="text-sm">
       <h1 className="text-3xl text-gray-500 font-bold uppercase">Hotels</h1>
-      <UserButton 
-      afterSignOutUrl="/sign-in"
+      <h1>Clerk User Id : {clerkUserId}</h1>
+      <h1>Clerk User Id : {name}</h1>
+      <h1>Clerk User Id : {email}</h1>
+      <UserButton
+        afterSignOutUrl="/sign-in"
       />
 
     </div>
