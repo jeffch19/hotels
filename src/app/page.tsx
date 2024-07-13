d36812801dd55477626d7036c514727c9778056c
jeffch19 <17jcummings@gmail.com>
2024-07-13 17:35:51 -0400

    sign up and sign in with clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { Button , Input} from "antd";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-5">
       <h1 className="text-3xl text-gray-500 font-bold uppercase">Hotels</h1>
-      <Button type="primary">Get Started</Button>
-      <Button type="default">Get Started</Button>
-      <Button type="link">Get Started</Button>
-      <Input placeholder="Username"/>
+      <UserButton 
+      afterSignOutUrl="/sign-in"
+      />
+
     </div>
   );
 }
