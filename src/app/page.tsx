385d73ad6b13ccb1083e2c8282c56cb287de9c8d
jeffch19 <17jcummings@gmail.com>
2024-07-14 14:53:05 -0400

    connected to mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { connectMongoDB } from "@/config/db";
 import { UserButton } from "@clerk/nextjs";
 import { currentUser } from '@clerk/nextjs/server';
+connectMongoDB();
 
 export default async function Home() {
   let clerkUserId = "";
@@ -15,8 +17,8 @@ export default async function Home() {
   }
 
   return (
-    <div className="text-sm">
-      <h1 className="text-3xl text-gray-500 font-bold uppercase">Hotels</h1>
+    <div className="text-sm flex flex-col gap-5 p-5">
+      <h1 className="text-3xl text-gray-500 font-bold uppercase">Homepage</h1>
       <h1>Clerk User Id : {clerkUserId}</h1>
       <h1>Clerk User Id : {name}</h1>
       <h1>Clerk User Id : {email}</h1>
