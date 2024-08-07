49a8ea8023d0f7d5cfca2c54c204b405ebff584f
jeffch19 <17jcummings@gmail.com>
2024-08-07 19:56:13 -0400

    firebase media upload logic

---
+++ src/helpers/image-upload.ts
@@ -0,0 +1,34 @@
+import { firebaseApp } from "@/config/firebase";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+export const uploadImageToFirebaseAndReturnUrls = async (files: File[]) => {
+  try {
+
+    //upload images to firebase
+    const storage = getStorage(firebaseApp);
+    const uploadedImagesRefs = await Promise.all(
+      files.map(async (file) => {
+        const storageRef = ref(storage, `images/${file.name}`);
+        await uploadBytes(storageRef, file);
+        return storageRef;
+      })
+    );
+
+
+
+    //get urls of images uploaded
+
+    const urls = await Promise.all(
+      uploadedImagesRefs.map(async (ref) => {
+        const url = await getDownloadURL(ref);
+        return url;
+      })
+    );
+
+    return urls;
+
+
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
