b235b8f245590797c6ca7d3c0c5408e007d748ba
jeffch19 <17jcummings@gmail.com>
2024-08-19 18:52:27 -0400

    adding loaders

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Spinner({fullHeight = false} : {fullHeight ?: boolean}) {
   return(
-    <div className="flex items-center justify-center" style={{ height: fullHeight ? "100vh" : "auto" }}>
+    <div className="flex items-center justify-center" style={{ height: fullHeight ? "80vh" : "auto" }}>
       <div className="border-solid h-10 w-10 border-8 border-t-gray-200 animate-spin rounded-full"></div>
     </div>
   )
